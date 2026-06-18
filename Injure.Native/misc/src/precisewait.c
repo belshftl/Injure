4e18a0be1f279f5c1cc27dbb15e4b66cd822c2c5
bel <belshftl@proton.me>
2026-06-19 03:06:10 +0500

    add spdx copyright info, remove license-name header from LICENSE since it's not actually the X Consortium variant

--- Injure.Native/misc/src/precisewait.c
+++ Injure.Native/misc/src/precisewait.c
@@ -1,4 +1,7 @@
-/* SPDX-License-Identifier: MIT */
+/*
+ * SPDX-FileCopyrightText: 2026 belshftl
+ * SPDX-License-Identifier: MIT
+ */
 
 /*
  * ============================================================================
